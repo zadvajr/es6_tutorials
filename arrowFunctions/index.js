bac501d6dca770fc0ec005b119815de1f7301374
zadvajr <danielzadva@gmail.com>
2024-08-07 00:29:07 +0100

    arrow functions

---
+++ arrowFunctions/index.js
@@ -0,0 +1,13 @@
+//Arrow Functions
+//arrow functions allow us to write shorter function syntax
+
+//before
+// function hello() {
+//   return "Hello World!";
+// }
+
+// document.write(hello());
+
+//after: with arrow functions
+hello = () => "Hello Arrow Function";
+document.write(hello());
